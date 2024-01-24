db821bbcdc64d705ff91286f3663fffbde32d36b
vignesh <vigneshkannan1467@gmail.com>
2024-01-24 13:13:00 +0530

    micro component created

--- src/app/ui/LegendContainer.tsx
+++ src/app/ui/LegendContainer.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 export interface propTypes {
-    heading: string
+    heading?: string
     para?: string
     button?: string
     children?: ReactNode
@@ -13,15 +13,15 @@ export interface propTypes {
 const LegendContainer = ({ children , heading, para, button, className } : propTypes) => {
     
   return (
-    <div className={twMerge("flex flex-col mt-5 gap-3 md:w-7/12", className)}>
-        <h1 className="font-bold leading-[38px] md:leading-[45px] lg:leading-[60px] text-[25px] md:text-[30px] lg:text-[48px] max-w-[540px}  pl-11 ml-20">
+    <div className={twMerge("flex flex-col mt-5 gap-3", className)}>
+        <h1 className="font-bold leading-[38px] md:leading-[45px] lg:leading-[60px] tracking-normal text-[#1D1D1D] text-[28px] md:text-[48px] lg:text-[48px] max-w-[540px}">
             {heading}
         </h1>
         {para && 
-            <p className="md:text-[20px] pl-11 ml-20" >{para}</p>
+            <p className="text-[16px] text-[#1D1D1D] leading-6 tracking-wide font-normal" >{para}</p>
         }
         { button &&
-            <button className="btn-primary py-12 md:py-2 h-[48px] w-full md:w-fit ml-32 bg-green-900 text-slate-100 ">button</button>
+            <button className=" flex px-10 py-6 flex-col items-center gap-2.5 self-stretch bg-[#017846] ">{button}</button>
         }
         {children}
     </div>
