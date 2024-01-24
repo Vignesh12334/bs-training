d07e36390a1f7b90b184582919392444cd861d69
vignesh <vigneshkannan1467@gmail.com>
2024-01-24 13:11:38 +0530

    Typography micro component created

---
+++ src/app/ui/Typography/HeadingL.tsx
@@ -0,0 +1,23 @@
+import { twMerge } from 'tailwind-merge'
+import React, { ReactNode } from 'react'
+import Image from 'next/image'
+
+
+export interface propTypes {
+    color?: string
+    children? : ReactNode
+    className? : string
+}
+
+
+
+const HeadingL = ({children, className} : propTypes) => {
+  return (
+        <h1 className={twMerge(' text-[#1D1D1D] text-[32px] not-italic font-bold leading-[38px]',className)}>
+            {children}
+        </h1>
+  )
+}
+
+// <HeadingL className="">Why our Product?</HeadingL>
+export default HeadingL
