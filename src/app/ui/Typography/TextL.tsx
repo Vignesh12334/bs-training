bec5c790b1434ca078126f88c5fc12e948dd1b94
vignesh <vigneshkannan1467@gmail.com>
2024-01-26 10:45:17 +0530

    textL changed

--- src/app/ui/Typography/TextL.tsx
+++ src/app/ui/Typography/TextL.tsx
@@ -13,7 +13,7 @@ export interface propTypes {
 
 const TextL= ({children, className} : propTypes) => {
   return (
-        <h1 className={twMerge(' text-[#1D1D1D] text-[16px] not-italic font-normal leading-[24px] tracking-[0.1px]',className)}>
+        <h1 className={twMerge(' text-[#1D1D1D] text-[16px] not-italic font-normal leading-[24px] tracking-[0.1px] md:text-[20px] md:leading-[24px]',className)}>
             {children}
         </h1>
   )
